b598569e3d5ef046e9a7f3c914db13411ea26ecb
ZTokbayev <ZTokbayev@Prog1>
2017-11-04 16:13:37 +0600

    041120171613

--- src/main/java/kz/ugs/webpush/sender/App.java
+++ src/main/java/kz/ugs/webpush/sender/App.java
@@ -15,6 +15,6 @@ public class App
     	String token = "fS_17t7lZNw:APA91bEcSN7rH-gWAO375e39uO90C1hYKkaF7a9BqqLEMvWiwwliy4deRW6OvNmVNTIr-_4XogFunYQU1Sxg6M7z_7begXIl_H__BugVwwFjk8Y61fDd3EokrcNbyYvV7XlIH-haa_7E";
     	String url = "https://e-smart.ugs.kz:8444/serviceworker/?token=fWc9hTafwks:APA91bH0YxgkLSNdBKpzgPhQ5rixuLxnGX2ck1igfMjS1wHsEXiS9pzYWkT7IQVWEvNumKNjMRLr9gw0N_j3ZKuIKLy3Os8wJSAw-uY-SKwNLwueKFLym5_-RbmRQSCHxgdlTyIzQhmT";
     	
-    	Sender sender = new Sender(key, token);
+    	Sender sender = new Sender(key, token, "115");
     }
 }
--- src/main/java/kz/ugs/webpush/sender/Sender.java
+++ src/main/java/kz/ugs/webpush/sender/Sender.java
@@ -5,36 +5,31 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class Sender {
 	
-	public Sender(String key, String token)	{
+	public Sender(String key, String token, String ticketNumber)	{
 		final String SERVER_KEY = key;
 		Pushraven.setKey(SERVER_KEY);
 		Notification raven = new Notification();
-		HashMap<String, Object> data = new HashMap<String, Object>();
-		data.put("Hello", "World!");
-		data.put("Marco", "Polo");
-		data.put("Foo", "Bar");
-		
 		AtomicInteger msgId = new AtomicInteger();
 		// build raven message using the builder pattern
 		raven.to(token)
 			.collapse_key("a_collapse_key")
 			.priority(1)
 			.delay_while_idle(true)
-			.time_to_live(100)
+			.time_to_live(1000)
 			//.restricted_package_name("false")
 			.dry_run(false)
-			.data(data)
-			.title("Testing")
-			.body("Hello World!")
-			.color("#ff0000");
+			//.data(data)
+			.title("SmartTime")
+			.body("Ваша очередь по билету №" + ticketNumber + " подошла.")
+			.color("#00e600");
 		//raven.addRequestAttribute("message_id", String.valueOf(msgId));
 
 		// push the raven message
-		FcmResponse response = Pushraven.push(raven);
+		//FcmResponse response = Pushraven.push(raven);
 
 		// alternatively set static notification first.
 		Pushraven.setNotification(raven);
-		response = Pushraven.push();
+		FcmResponse response = Pushraven.push();
 
 		// prints response code and message
 		System.out.println(response);
